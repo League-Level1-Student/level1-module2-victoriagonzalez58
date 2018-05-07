8096232e5bf57c8c4c46969d800da3a0aec456ce
victoriagonzalez58 <Victoria.Gonzalez-Calderon@gompersstudent.org>
2018-05-07 14:25:41 -0700

    Add files via upload

---
+++ src/Miniontestrunner.java
@@ -0,0 +1,18 @@
+
+public class Miniontestrunner {
+	String name;
+	int eyes;
+	String color;
+	String master;
+	
+public Miniontestrunner(String n, String c, String m, int e){
+	
+	n=name;
+	c=color;
+	m=master;
+	e=eyes;
+}
+public static void main(String[] args) {
+	Miniontestrunner kyle = new Miniontestrunner("kyle","yellow","george",2);
+}
+}
